ec2289a54d89578c9192390db2bbeb9a5795028c
matheusmelos <mtheusmelo.s@gmail.com>
2025-06-15 20:39:59 -0300

    Commit 6 - Fatura em HTML

--- index.js
+++ index.js
@@ -76,7 +76,27 @@ function gerarFaturaStr(fatura, pecas) {
     return faturaStr;
 }
 
+function gerarFaturaHTML(fatura, pecas) {
+    let faturaHTML = "<html>\n";
+    faturaHTML += `<p> Fatura ${fatura.cliente} </p>\n`;
+    faturaHTML += "<ul>\n";
+
+    for (let apre of fatura.apresentacoes) {
+        faturaHTML += `<li> ${getPeca(apre, pecas).nome}: ${formatarMoeda(calcularTotalApresentacao(apre, pecas))} (${apre.audiencia} assentos) </li>\n`;
+    }
+
+    faturaHTML += "</ul>\n";
+    faturaHTML += `<p> Valor total: ${formatarMoeda(calcularTotalFatura(fatura, pecas))} </p>\n`;
+    faturaHTML += `<p> Créditos acumulados: ${calcularTotalCreditos(fatura, pecas)} </p>\n`;
+    faturaHTML += "</html>";
+
+    return faturaHTML;
+}
+
+
 const faturas = JSON.parse(readFileSync('./faturas.json'));
 const pecas = JSON.parse(readFileSync('./pecas.json'));
 const faturaStr = gerarFaturaStr(faturas, pecas);
 console.log(faturaStr);
+const faturaHTML = gerarFaturaHTML(faturas, pecas);
+console.log(faturaHTML);
